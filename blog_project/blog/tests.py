f06404d6638ed78105025711e7e2106329af89da
SandipanGhosh <sandipan.ghosh2005@gmail.com>
2018-03-05 12:46:38 -0500

    Added unit test case for Post.

--- blog_project/blog/tests.py
+++ blog_project/blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from blog.models import Post
 
 # Create your tests here.
+class PostTests(TestCase):
+
+	def test_str(self):
+		my_title = Post(title='Test case title')
+		self.assertEquals(
+			str(my_title), 'Test case title',
+			)
